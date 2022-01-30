2dbff4df8f28df620ca886815fdbd57552f6b3ab
David_Elout <dedavid.elout@gmail.com>
2022-01-30 11:45:30 +0100

    Enemy deatgh particle, and trail to player

---
+++ Assets/Level/ParticleToPlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ParticleToPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offSet = new Vector3(0f,3f,0f);
+
+    public ParticleSystem[] flame;
+    public GameObject trail;
+
+    public float range;
+    public float speed = 5f;
+
+    bool isActive = false;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < range && !isActive)
+        {
+            for (int i = 0; i < flame.Length; i++)
+            {
+                flame[i].Stop(false, ParticleSystemStopBehavior.StopEmitting);
+            }
+
+            isActive = true;
+        }
+
+        if (isActive)
+        {
+            trail.transform.DOMove(player.transform.position+offSet, speed);
+        }
+    }
+
+
+    //Cope this in player script
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Matancy"))
+    //    {
+    //        //Increase matancy of player
+
+    //        Destroy(other.transform.parent);
+    //    }
+    //}
+
+}
