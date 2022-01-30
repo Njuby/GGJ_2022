850b870623f01a8c5a833245a043bc5f959dea47
Stanley <stanley.ursem@hotmail.com>
2022-01-30 01:53:19 +0100

    Added mutant transform vfx

---
+++ Assets/MutantForm/MutantForm.cs
@@ -0,0 +1,61 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class MutantForm : MonoBehaviour
+{
+    [SerializeField] private float Interval = 0.5f;
+    [SerializeField] private Mutant mutant;
+    [SerializeField] private SkinnedMeshRenderer playerRend;
+    [SerializeField] private MeshRenderer pistolRend;
+    [SerializeField] private PlayerMaterials humanMaterials;
+    [SerializeField] private PlayerMaterials mutantMaterials;
+    [SerializeField] private VisualEffect gunEffect;
+    [SerializeField] private VisualEffect transformEffect;
+    [SerializeField] private VisualEffect detransformEffect;
+    private Sequence detrans;
+    private Sequence trans;
+
+    private void Start()
+    {
+        detrans = DOTween.Sequence();
+        trans = DOTween.Sequence();
+        mutant.OnMutationModeActivated += Mutant_OnMutationModeActivated;
+        mutant.OnMutationModeDectivated += Mutant_OnMutationModeDectivated;
+    }
+
+    private void Mutant_OnMutationModeDectivated()
+    {
+        detransformEffect.enabled = true;
+        transformEffect.enabled = false;
+        detransformEffect.Play();
+        if (detrans.IsActive() || detrans.IsPlaying())
+        {
+            detrans.Kill();
+        }
+        detrans = DOTween.Sequence();
+
+        detrans.AppendInterval(Interval);
+        detrans.AppendCallback(() => humanMaterials.SetMaterials(playerRend, pistolRend));
+
+        gunEffect.enabled = false;
+    }
+
+    private void Mutant_OnMutationModeActivated()
+    {
+        transformEffect.enabled = true;
+        detransformEffect.enabled = false;
+        gunEffect.enabled = true;
+
+        transformEffect.Play();
+        if (trans.IsActive() || trans.IsPlaying())
+        {
+            trans.Kill();
+        }
+        trans = DOTween.Sequence();
+        trans.AppendInterval(Interval);
+        trans.AppendCallback(() => mutantMaterials.SetMaterials(playerRend, pistolRend));
+    }
+}
---
+++ Assets/MutantForm/PlayerMaterials.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/PlayerMaterials", fileName = "PlayerMaterials")]
+public class PlayerMaterials : ScriptableObject
+{
+    public Material hairMat;
+    public Material bodyMat;
+    public Material headMat;
+    public Material eyesMat;
+    public Material tearMat;
+    public Material pistolMat;
+
+    public void SetMaterials(SkinnedMeshRenderer body, MeshRenderer pistol)
+    {
+        Material[] materials = new Material[6];
+
+        materials[0] = hairMat;
+        materials[1] = bodyMat;
+        materials[2] = headMat;
+        materials[3] = eyesMat;
+        materials[4] = tearMat;
+        materials[5] = hairMat;
+
+        body.materials = materials;
+
+        pistol.material = pistolMat;
+    }
+}
