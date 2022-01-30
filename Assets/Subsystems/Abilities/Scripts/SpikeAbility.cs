2c41e39507c97cac9679d0d131abbf5b3709e9e6
Johannes <Johannes@trivetechnology.com>
2022-01-30 16:12:04 +0100

    here you go

--- Assets/Subsystems/Abilities/Scripts/SpikeAbility.cs
+++ Assets/Subsystems/Abilities/Scripts/SpikeAbility.cs
@@ -8,23 +8,32 @@ public class SpikeAbility : Ability
     public GameObject spike;
     public int spikeAmount;
 
+    public IntEvent useAbilityEvent;
+
     public IntEvent mutantStrengthEvent;
+    public BoolEvent mutantChangeEvent;
 
     [ColorUsage(true, true)] public Color color1;
     [ColorUsage(true, true)] public Color color2;
 
     public int mutantcy;
+    public bool active;
 
     public void OnEnable()
     {
         mutantStrengthEvent.Register((x) => mutantcy = x);
+        mutantChangeEvent.Register((x) => active = x);
     }
 
     public override void DoAbility()
     {
+        if (!active) return;
+
         base.DoAbility();
 
-        for (int i = 0; i < spikeAmount * mutantcy; i++)
+        useAbilityEvent.Raise(5);
+
+        for (int i = 0; i < spikeAmount * (mutantcy / 5); i++)
         {
             GameObject obj = PoolManager.Instance.GetFromPool(spike, transform.position + Vector3.up, Quaternion.Euler(0, 360f / (spikeAmount * mutantcy) * i, 0));
             obj.GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", Color.Lerp(color1, color2, Random.value));
--- Assets/Subsystems/Player.cs
+++ Assets/Subsystems/Player.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class Player : Hittable
 {
+
+    public BoolEvent MutantSwitch;
+    public VoidEvent DamageTakenFromMutant;
+
+    public float time;
+    public float timeTillHeal;
+
+    public bool mutant;
+
+    public void Start()
+    {
+        DamageTakenFromMutant.Register(() => TakeDamage(1));
+        MutantSwitch.Register((x) => mutant =x);
+    }
+
     public override void Die()
     {
         Destroy(gameObject);
+    }
+
+    public void Update()
+    {
+        if (mutant) return;
+
+        time += Time.deltaTime;
+        if(time > timeTillHeal)
+        {
+            time = 0;
+            if(Health < MaxHealth)
+                TakeDamage(-1);
+        }
     }
 }
